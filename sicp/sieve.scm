f30a33791d88064024a60e123cd5c911ea786462
JD Huntington <jdhuntington@gmail.com>
2009-03-15 10:54:03 -0700

    Prime number sieve using scheme streams

---
+++ sicp/sieve.scm
@@ -0,0 +1,16 @@
+(define (divisible? x y)
+  (= (remainder x y) 0))
+
+(define (integers-starting-from n)
+ (cons-stream n 
+  (integers-starting-from (+ n 1))))
+
+(define (sieve stream)
+   (cons-stream
+     (stream-car stream)
+     (sieve 
+       (stream-filter
+         (lambda (x) (not (divisible? x (stream-car stream))))
+         (stream-cdr stream)))))
+
+(define primes (sieve (integers-starting-from 2)))
